b53f3a82b0d675bc2a5029090c390c9d60015a57
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-26 13:52:04 +0530

    Adds initial client stack setup

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { Router, Route, IndexRoute, useRouterHistory, IndexRedirect } from 'react-router';
+import { createHashHistory } from 'history';
+
+const appHistory = useRouterHistory(createHashHistory)({ queryKey: false });
+
+import App from '../App.js';
+
+import Home from './Home.jsx';
+
+const Routes = (
+  <Router history={appHistory}>
+    <Route path = "/" component = {App}>
+      <IndexRoute component = {Home} />
+      <Route path = "home" component = {Home} />
+    </Route>
+  </Router>
+);
+
+export default Routes;
