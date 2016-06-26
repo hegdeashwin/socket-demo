77d5c823af2e6bfaafafe8fa86e642e41cb1bdcc
hegdeashwin <ashwin.hegde3@gmail.com>
2016-06-26 14:09:12 +0530

    Adds server stack

---
+++ server/stack.conf.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = {
+  "server": {
+    "dist": {
+      "port": 8000,
+      "ip": "127.0.0.1",
+      "codebase": "../dist",
+      "NODE_ENV": "production"
+    }
+  }
+};
